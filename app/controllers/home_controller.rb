bed670c7f116cd4f55858f8c8e15e862ae7f5a64
Surve <surve8729@gmail.com>
2025-01-22 21:11:38 +0530

    Fetching data into controller from the anilist_service

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    data = AnilistService.fetch_top_anime_and_manga
+    @top_anime = data["anime"]["media"]
+    @top_manga = data["manga"]["media"]
+  rescue StandardError => e
+    @error = "An error occured: #{e.message}"
   end
 end
